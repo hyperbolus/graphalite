2ed9c5cf75575c56546b6505458d23fe4b5c9864
0x1DEA <seebeyondedm@gmail.com>
2023-11-11 19:05:56 -0600

    0.2.0 Proper exports! and other cleanups.

---
+++ index.ts
@@ -0,0 +1,13 @@
+import {Renderer} from "./src/Renderer";
+import {Level} from "./src/Level";
+import {Texture} from "./src/Texture";
+import {Camera} from "./src/Camera";
+import {Annotation} from "./src/Annotation";
+
+export {
+    Renderer,
+    Level,
+    Texture,
+    Camera,
+    Annotation
+}
